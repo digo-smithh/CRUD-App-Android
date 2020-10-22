37ab578c557f3dee3fb65bd341a94042e24b7636
digo-smithh <smithrodrigues08@gmail.com>
2020-10-22 18:50:20 -0300

    First config

--- App/app/src/main/java/com/project/studentsapp/MainActivity.java
+++ App/app/src/main/java/com/project/studentsapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.project.studentsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
