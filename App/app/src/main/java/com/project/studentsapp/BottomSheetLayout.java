933cf03700900cff0c553abb11bd9aae97dcbdc8
digo-smithh <smithrodrigues08@gmail.com>
2020-10-25 17:09:32 -0300

    Adjustments

---
+++ App/app/src/main/java/com/project/studentsapp/BottomSheetLayout.java
@@ -0,0 +1,17 @@
+package com.project.studentsapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class BottomSheetLayout extends BottomSheetDialogFragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.bottom_sheet_layout, container, false);
+    }
+}
--- App/app/src/main/java/com/project/studentsapp/MainActivity.java
+++ App/app/src/main/java/com/project/studentsapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.project.studentsapp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
 
@@ -13,6 +14,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final BottomAppBar bottomAppBar = findViewById(R.id.bottomAppBar);
+        setSupportActionBar(bottomAppBar);
 
+        bottomAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final BottomSheetLayout bottomSheetLayout = new BottomSheetLayout();
+                bottomSheetLayout.show(getSupportFragmentManager(), bottomSheetLayout.getTag());
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.bottom_app_bar_menu, menu);
+        return true;
     }
 }
