823a303d3849b4c8db530635fb38801c55cf4304
digo-smithh <smithrodrigues08@gmail.com>
2020-10-27 18:14:39 -0300

    Adjustments

--- App/app/src/main/java/com/project/studentsapp/app/MainActivity.java
+++ App/app/src/main/java/com/project/studentsapp/app/MainActivity.java
@@ -2,12 +2,16 @@ package com.project.studentsapp.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.project.studentsapp.R;
 import com.project.studentsapp.data.Student;
 
@@ -17,19 +21,45 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     List<Student> studentList;
+    int currentView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        currentView = R.layout.activity_main;
+        setContentView(currentView);
+        buildMainView();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.bottom_app_bar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (currentView == R.layout.register_student_layout) {
+            currentView = R.layout.activity_main;
+            setContentView(currentView);
+            buildMainView();
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+
+    private void buildMainView() {
+        studentList = new ArrayList<>();
 
         final BottomAppBar bottomAppBar = findViewById(R.id.bottomAppBar);
         setSupportActionBar(bottomAppBar);
 
+        final FloatingActionButton floatingActionButton = findViewById(R.id.button);
+
         final ProgressBar progressBar = findViewById(R.id.progressBar);
         progressBar.setVisibility(View.INVISIBLE);
-
-        studentList = new ArrayList<>();
 
         bottomAppBar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,12 +68,14 @@ public class MainActivity extends AppCompatActivity {
                 bottomSheetActivity.show(getSupportFragmentManager(), bottomSheetActivity.getTag());
             }
         });
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.bottom_app_bar_menu, menu);
-        return true;
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentView = R.layout.register_student_layout;
+                setContentView(currentView);
+            }
+        });
     }
 
     private void updateView() {
